186aa46854f647c5845a64f09ac2dd0f7e739969
malfuncti0n <savvaniss@yahoo.gr>
2017-03-13 16:26:05 +0200

    create user and show schema and models also established connection to database

--- server.js
+++ server.js
@@ -1,10 +1,81 @@
+//express requirements
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
+//run express
 var app = express();
+
+//mongo scheemas(mongoose)
+
+//show scheema
+var showSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  airsDayOfWeek: String,
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  rating: Number,
+  ratingCount: Number,
+  status: String,
+  poster: String,
+  subscribers: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  }],
+  episodes: [{
+      season: Number,
+      episodeNumber: Number,
+      episodeName: String,
+      firstAired: Date,
+      overview: String
+  }]
+});
+
+//user scheema
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+//models
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+
+
+//connect to database
+mongoose.connect("mongodb://localhost:27017/show", function(err,db){
+    if(!err){
+        console.log("we are connected to mongo");
+    }
+})
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
